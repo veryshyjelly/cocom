3d3f89e9645b05ddfa41ad0d3c1d9703079c7e8c
Ayush Biswas <biswas.ayush03@kgpian.iitkgp.ac.in>
2026-06-28 18:26:27 +0530

    finalized config file structure

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	tea "charm.land/bubbletea/v2"
+)
+
+func main() {
+	p := tea.NewProgram(initialModel())
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("Alas, there's been an error: %v", err)
+		os.Exit(1)
+	}
+}
